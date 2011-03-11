35d715e0b736ac0bf1cbdb8ef5d46f51e57df536
Arvind <arvind@servelots.com>
2011-03-12 00:08:31 +0530

    Adding new features, fixed a dirty hack

--- ffext/chrome/content/overlay.js
+++ ffext/chrome/content/overlay.js
@@ -6,10 +6,14 @@ var a11ypi = {
   },
 
   onMenuItemCommand: function(e) {
-    var promptService = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
-                                  .getService(Components.interfaces.nsIPromptService);
-    promptService.alert(window, this.strings.getString("helloMessageTitle"),
-                                this.strings.getString("helloMessage"));
+      var ren = document.getElementById("a11ypi-txt");
+      ren.value = content.document.getElementById(e.target.value).textContent;
+      var foruri = document.getElementById("a11ypi-foruri");
+      foruri.value = content.window.location + ':' + e.target.value;
+    //   var promptService = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
+    //                               .getService(Components.interfaces.nsIPromptService);
+    // promptService.alert(window, this.strings.getString("helloMessageTitle"),
+    //                             this.strings.getString("helloMessage"));
   },
 
   onToolbarButtonCommand: function(e) {
@@ -31,6 +35,24 @@ var a11ypi = {
     // 		alert(e.name);
     // 	    }
     // }
+    onSidebarinput: function(e){
+	alert(e.value);
+	var sel = content.window.getSelection().getRangeAt(0).cloneContents();
+	// var ht = content.document.createRange();
+	// ht.selectNode(getElementsByTagName("body"));
+	// var sel = ht.cloneContents();
+	var x = content.document.createElement("test");
+	x.appendChild(sel);
+	alert(x.innerHTML);
+	y = x.getElementsByTagName("*");
+	for (var i=0;i<y.length;i++)
+	{
+	    if(y[i].getAttribute("id")!=null)
+	    {
+		alert(y[i].getAttribute("id"));
+	    }
+	}
+    },
     getURL: function(e) {
 	var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"]     //The service branch which handles the "Window".
 	.getService(Components.interfaces.nsIWindowMediator);
@@ -68,10 +90,10 @@ var a11ypi = {
 	return "True";
     },
     createMenu: function(menu_list) {
-	xyz = document.getElementById("menu-button");
+	var xyz = document.getElementById("menu-button");
 	for(var i in menu_list)
 	    {
-		newel = document.createElement("menuitem");
+		var newel = document.createElement("menuitem");
 		newel.setAttribute("label",menu_list[i]);
 		newel.setAttribute("value",menu_list[i]);
 		newel.setAttribute("oncommand","a11ypi.getURL(event.target);");
@@ -79,12 +101,35 @@ var a11ypi = {
 	    }
     },
     clearMenu: function() {
-	xyz = document.getElementById("menu-button");
-	for(var i=0;i<(xyz.childNodes.length);i++)
-	    {
-		xyz.removeChild(xyz.childNodes[i]);
-	    }
-	xyz.removeChild(xyz.childNodes[0]);                   //A dirty hack for making the drop down work.
+	var xyz = document.getElementById("menu-button");
+	while(null!= xyz.firstChild)
+	{
+	    xyz.removeChild(xyz.firstChild);
+	}
     },
+    replacement: function()
+    {
+	var xyz = document.getElementById("replacement_id");
+	var xy = document.getElementById("replacement_pop");
+	xy.addEventListener("command", a11ypi.onMenuItemCommand ,false);  
+	var divs = content.document.getElementsByTagName("*");
+	for(var i=0;i<divs.length;i++)
+	{
+	    if(divs[i].getAttribute("id")!=null)
+	    {
+		xyz.appendItem(divs[i].getAttribute("id"),divs[i].getAttribute("id"));
+	     }
+	}
+    },
+ clearReplacement: function() {
+     var xy = document.getElementById("replacement_pop");
+     xy.removeEventListener("command", a11ypi.onMenuItemCommand,false);
+     while(null!= xy.firstChild)
+     {
+	 xy.removeChild(xy.firstChild);
+     }
+     var xyz = document.getElementById("replacement_id");
+     xyz.setAttribute("label","Select one id"); 
+},
 };
 window.addEventListener("load", function () { a11ypi.onLoad(); }, false);
